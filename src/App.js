c53950faaf7c4ce7b929d77363a86b6ea12e8d0e
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-09-29 01:35:15 +1000

    Updated route

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
           }
         />
         <Route
-          path={"/cart/orderItems/:_id"}
+          path={"/cart/order/:_id"}
           element={
             <PrivateRoute>
               <OrderConfirmationPage />
